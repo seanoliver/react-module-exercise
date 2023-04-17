52c01dc99411ae2dbe80d11b89031aa548885ebd
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-04-17 14:54:45 -0700

    finished project

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,14 @@
+// returns a randomly selected item from array of items
 function choice(items) {
-  // returns a randomly selected item from array of items
-  
+  const rand = Math.floor(Math.random() * items.length);
+  return items[rand];
 }
 
+// removes the first matching item from items, if item exists, and returns it.
+// Otherwise returns undefined.
 function remove(items, item) {
-  // removes the first matching item from items, if item exists, and returns it. Otherwise returns undefined.
-
+  const idx = items.findIndex(i => i === item);
+  return (idx > 0) ? items.splice(idx, 1) : undefined;
 }
 
 export { choice, remove };
--- src/index.js
+++ src/index.js
@@ -6,3 +6,13 @@
 // Log the message “Delicious! May I have another?”
 // Remove the fruit from the array of fruits
 // Log the message “I’m sorry, we’re all out. We have FRUITSLEFT left.”
+
+import { fruits } from "./foods.js";
+import { choice, remove } from "./helpers.js";
+
+let randomFruit = choice(fruits);
+console.log(`I'd like one ${randomFruit}, please.`);
+console.log(`Here you go: ${randomFruit}`);
+console.log(`Delicious! May I have another?`);
+remove(fruits, randomFruit);
+console.log(`I'm sorry. We're all out. We have ${fruits} left.`);
