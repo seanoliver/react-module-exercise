508074f58075f35e43468dfff04442f9675d5bd7
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-04-17 14:27:03 -0700

    setup

---
+++ src/helpers.js
@@ -0,0 +1,11 @@
+function choice(items) {
+  // returns a randomly selected item from array of items
+  
+}
+
+function remove(items, item) {
+  // removes the first matching item from items, if item exists, and returns it. Otherwise returns undefined.
+
+}
+
+export { choice, remove };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// This file should import the fruits and both array helpers. It should then:
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Randomly draw a fruit from the array
+// Log the message “I’d like one RANDOMFRUIT, please.”
+// Log the message “Here you go: RANDOMFRUIT”
+// Log the message “Delicious! May I have another?”
+// Remove the fruit from the array of fruits
+// Log the message “I’m sorry, we’re all out. We have FRUITSLEFT left.”
